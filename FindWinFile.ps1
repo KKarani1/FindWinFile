55da79696b9d3e735ebf57ac65226c9ad6b71500
KKarani <kkarani1@outlook.com>
2022-08-27 22:16:04 +0000

    Upload New File

---
+++ FindWinFile.ps1
@@ -0,0 +1,2 @@
+#Script to find the first instance of a file (in this case any zip file) in the C Drive. Also configured to ignore errors.
+Get-ChildItem -Path C:\ *.zip -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1
